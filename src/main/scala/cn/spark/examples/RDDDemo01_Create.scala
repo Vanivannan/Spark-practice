0cbfb033e4b7bb952aaf166322f75f6f442c25e1
Vanivannan <776169038@qq.com>
2022-04-01 18:46:39 +0800

    update code

--- src/main/scala/cn/spark/examples/RDDDemo01_Create.scala
+++ src/main/scala/cn/spark/examples/RDDDemo01_Create.scala
@@ -5,15 +5,41 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object RDDDemo01_Create {
   def main(args: Array[String]): Unit = {
-    //todo -1.env
-    val conf: SparkConf = new SparkConf().setAppName("sparkExamples").setMaster("local[*]")
+    //TODO -1.env/创建环境
+    val conf: SparkConf = new SparkConf().setAppName("spark").setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
-    //todo -2.load source
-    val rdd1: RDD[Int] = sc.parallelize(1 to 100)
+    //TODO -2.source/加载数据/创建RDD
+    val rdd1: RDD[Int] = sc.parallelize(1 to 10)
+    val rdd2: RDD[Int] = sc.parallelize(1 to 10,3)
 
-    //todo -3.输出
-    println(rdd1.getNumPartitions)
+    val rdd3: RDD[Int] = sc.makeRDD(1 to 10)
+    val rdd4: RDD[Int] = sc.makeRDD(1 to 10,4)
+
+    val rdd5: RDD[String] = sc.textFile("data/input/words.txt")
+    val rdd6: RDD[String] = sc.textFile("data/input/words.txt",3)
+
+    val rdd7: RDD[String] = sc.textFile("data/input/ratings10")
+    val rdd8: RDD[String] = sc.textFile("data/input/ratings10",3)
+
+    val rdd9: RDD[(String, String)] = sc.wholeTextFiles("data/input/ratings10")
+    val rdd10: RDD[(String, String)] = sc.wholeTextFiles("data/input/ratings10",3)
+
+    println(rdd1.getNumPartitions)//底层为 partitions.length
+    println(rdd2.partitions.length)
+    println(rdd3.getNumPartitions)
+    println(rdd4.getNumPartitions)
+    println(rdd5.getNumPartitions)
+    println(rdd6.getNumPartitions)
+    println(rdd7.getNumPartitions)
+    println(rdd8.getNumPartitions)
+    println(rdd9.getNumPartitions)
+    println(rdd10.getNumPartitions)
+
+    //TODO -3.transformation
+
+    //TODO -4.sink/输出
+
   }
 }
