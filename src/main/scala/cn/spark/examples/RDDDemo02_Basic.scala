278ecc3a45c7ed8728c6b815070b50aebbe8065b
Vanivannan <776169038@qq.com>
2022-04-18 09:50:24 +0800

    clean target

---
+++ src/main/scala/cn/spark/examples/RDDDemo02_Basic.scala
@@ -0,0 +1,34 @@
+package cn.spark.examples
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object RDDDemo02_Basic {
+  def main(args: Array[String]): Unit = {
+    //TODO -1.env/创建环境
+    val conf: SparkConf = new SparkConf().setAppName("spark").setMaster("local[*]")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    //TODO -2.source/加载数据/创建RDD
+    val lines: RDD[String] = sc.textFile("data/input/words.txt")
+
+    //TODO -3.transformation
+    val result: RDD[(String, Int)] = lines.filter(StringUtils.isNoneBlank(_))
+      .flatMap(_.split(" "))
+//      .map((_, 1))
+      // f: Iterator[T] => Iterator[U]
+      .mapPartitions(iter=>{
+        iter.map((_,1))
+      })
+      .reduceByKey(_ + _)
+
+    //TODO -4.sink/输出
+    result.foreach(println)
+    result.foreachPartition(iter=>{
+      iter.foreach(println)
+    })
+//    result.saveAsTextFile("data/output/result1")
+  }
+}
