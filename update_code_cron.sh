0183abe3c83a7fc79051a3020c3dfa23d34b6247
Vanivannan <776169038@qq.com>
2022-07-05 22:20:28 +0800

    update file

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 export LANG=zh_CN.UTF-8
-export source_path=/opt/opensource/Spark-practice
+source_path=/opt/opensource/Spark-practice
 
 # 0,30 * * * * /bin/sh /opt/opensource/studyspace/update_code_cron.sh 2>&1 >/opt/opensource/studyspace/$(date +\%Y\%m\%d).log
 echo ''
 echo "update time: `date`"
 echo '**********************************START************************************************' 1>&2
 cd ${source_path}
-#git stash
-#git pull -u origin master
 echo `date`>>tip.txt
-git add --all
-#git add .
-git commit -m "update file"
-git push -u origin master
-git stash
-git pull -u origin master
-echo `git status` 1>&2
+git status
+if [ $? -eq 0 ]; then
+    	echo "提交代码到github"
+	git add --all && git commit -m "update file"
+	git push -u origin master
+    	echo "提交代码成功！！！"
+   else
+   	 echo "提交代码失败！！！"
+fi
+git stash && git pull -u origin master
+if [ $? -eq 0 ]; then
+    echo "拉取最新的代码！！！"
+   else
+    echo "拉取最新的代码失败！！！"
+   fi
 echo '***********************************END**************************************************' 1>&2
