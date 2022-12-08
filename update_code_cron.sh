8bde58da20d38ac009057247f3fb0e8ccde37aeb
Vanivannan <776169038@qq.com>
2022-12-09 01:16:01 +0800

    update file

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -9,7 +9,7 @@ echo "update time: `date`"
 echo '**********************************************************************************'
 cd ${source_path}
 echo "update code date is: `date`" >>tip.txt
-/usr/git/bin/git add . && /usr/git/bin/git commit -m "update file"
-/usr/git/bin/git push origin master
-/usr/git/bin/git stash && git pull origin master
+git add . && git commit -m "update file"
+git push origin master
+git stash && git pull origin master
 echo '**********************************************************************************'
